9e731d87d4d1443028aed525ded7f6d451162af6
Florian J. Metwaly <152606616+flo1met@users.noreply.github.com>
2025-02-05 14:12:43 +0100

    edit tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -6,13 +6,16 @@
 using TargetTrialEmulation
 using Test
 using DataFrames
-using CodecBzip2
-using RData
 using CSV
 using GLM
+using CategoricalArrays
 
-test_df = CSV.read("../data/seq_emulated_data_censored.csv", DataFrame)
-df = RData.load("../data/data_censored.rda")["data_censored"]
+test_df = CSV.read("../data/data_censored_sequential.csv", DataFrame)
+df = CSV.read("../data/data_censored.csv", DataFrame)
+
+df[!, :x1] = CategoricalVector(df.x1)
+df[!, :x3] = CategoricalVector(df.x3)
+
 out_df = seqtrial(df, [:x1, :x2, :x3, :x4, :age])
 
 #sort the dfs
@@ -24,19 +27,21 @@ test_df = sort(test_df, [:id, :trial_period])
     @test nrow(out_df) == nrow(test_df)
     @test out_df.id == test_df.id
     @test out_df.trialnr == test_df.trial_period
-    @test isapprox(out_df.x1, test_df.x1, atol = 0.01)
-    @test isapprox(out_df.x2, test_df.x2, atol = 0.01)
-    @test isapprox(out_df.x3, test_df.x3, atol = 0.01)
-    @test isapprox(out_df.x4, test_df.x4, atol = 0.01)
-    @test out_df.age == test_df.age
+    @test isapprox(out_df.x1_first, test_df.x1, atol = 0.01)
+    @test isapprox(out_df.x2_first, test_df.x2, atol = 0.01)
+    @test isapprox(out_df.x3_first, test_df.x3, atol = 0.01)
+    @test isapprox(out_df.x4_first, test_df.x4, atol = 0.01)
+    @test out_df.age_first == test_df.age
     @test out_df.outcome == test_df.outcome
     @test out_df.baseline_treatment == test_df.assigned_treatment
     @test out_df.fup == test_df.followup_time
 end
-
-m_out = glm(@formula(outcome ~ x1 + x2 + x3 + x4 + age + baseline_treatment + fup + fup^2 + trialnr + trialnr^2), out_df, Binomial(), LogitLink())
+# R outcome model test
+m_out = glm(@formula(outcome ~ x1_first + x2_first + x3_first + x4_first + age_first + treatment_first + fup + fup^2 + trialnr + trialnr^2), out_df, Binomial(), LogitLink())
 m_test = glm(@formula(outcome ~ x1 + x2 + x3 + x4 + age + assigned_treatment + followup_time + followup_time^2 + trial_period + trial_period^2), test_df, Binomial(), LogitLink())
 
 @testset "outcome model parameters" begin
     @test isapprox(coef(m_out), coef(m_test), atol = 0.0001)
 end
+
+# test censoring models
